68e59bc3c5cb05b651183d8939a8b4bfb66753fb
Shaliko Usubov <shaliko@ezid.ru>
2013-06-14 23:06:30 +0400

    Force set twitter api version

--- lib/twitter_oauth/account.rb
+++ lib/twitter_oauth/account.rb
@@ -4,13 +4,13 @@ module TwitterOAuth
     # Returns an HTTP 200 OK response code and a representation of the requesting user if authentication was successful;
     # returns a 401 status code and an error message if not.
     def authorized?
-      oauth_response = access_token.get('/1/account/verify_credentials.json')
+      oauth_response = access_token.get('/1.1/account/verify_credentials.json')
       return oauth_response.class == Net::HTTPOK
     end
 
     # Returns client info
     def info
-      get('/account/verify_credentials.json')
+      get('/1.1/account/verify_credentials.json')
     end
 
     # Returns the remaining number of API requests available to the requesting user before the API limit is reached for the current hour.
